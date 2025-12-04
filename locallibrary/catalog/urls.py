174365f6cb8ec7ddf7b027b3d3e2c9ac32b5e213
Artem <qu1sty@MacBook-Pro-4.local>
2025-12-04 13:53:25 +0700

    first commit

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('author/', views.AuthorListView.as_view(), name='author'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('borrowed-all/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
